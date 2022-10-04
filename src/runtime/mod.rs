8759e78f5c71493ab02ec330c79c85c42caab921
Thomas <31560900+0xtlt@users.noreply.github.com>
2022-10-04 21:09:44 +0200

    var tmp

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -18,7 +18,20 @@ fn exec(instruction: &Instruction, variables: &mut LiquidVariableType, end_str: 
             manipulation_mode,
             data,
             manipulations,
-        ) => {}
+        ) => {
+            // Get the variable pointer in the var_tmp variabke
+            // TODO: do it
+            let mut var_tmp = match manipulation_mode {
+                crate::jit::data_manipulation::DataManipulationMode::Assign => todo!(),
+                crate::jit::data_manipulation::DataManipulationMode::Echo => todo!(),
+            };
+
+            // Manipulate the variable
+            // TODO: do it
+
+            // If manipulation mode is echo, push the variable to the end_str
+            // TODO: do it
+        }
     }
 }
 
