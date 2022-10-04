bac00cdff32dbeec30dcdf3f41a8c4ce82da9e43
Thomas <31560900+0xtlt@users.noreply.github.com>
2022-10-04 21:31:50 +0200

    Update fast_var.rs

--- src/jit/fast_var.rs
+++ src/jit/fast_var.rs
@@ -1,5 +1,20 @@
-use super::types::Instruction;
+use super::types::{FastVarFinder, Instruction};
+
+pub fn create_fast_var(path: &str) -> Vec<FastVarFinder> {
+    todo!()
+}
 
 // Pass instructions in a function to split variables into a fast find map
 // TODO: do it
-pub fn fast_var_process(instructions: &mut [Instruction]) {}
+pub fn fast_var_process(instructions: &mut [Instruction]) {
+    for instruction in instructions.iter_mut() {
+        match &mut instruction.op_type {
+            crate::jit::types::InstructionType::Raw(_) => todo!(),
+            crate::jit::types::InstructionType::DataManipulation(_, data, _) => {
+                if let crate::jit::types::VarOrRaw::Var(name) = data {
+                    *data = crate::jit::types::VarOrRaw::FastVar(create_fast_var(name));
+                }
+            }
+        }
+    }
+}
