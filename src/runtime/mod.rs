47f5d015013883bef9e8919f581fe4a51eff9420
Thomas <31560900+0xtlt@users.noreply.github.com>
2022-10-04 21:16:50 +0200

    Update mod.rs

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -5,9 +5,27 @@ use std::collections::HashMap;
 
 use serde_json::Value;
 
-use crate::jit::types::{Instruction, Instructions};
+use crate::jit::types::{Instruction, Instructions, VarOrRaw};
 
 use self::types::LiquidVariableType;
+
+// Path can be : "a.b.c" or "a[0].b.c"
+fn get_variable<'a>(
+    variables: &'a mut LiquidVariableType,
+    path: &'a str,
+) -> Option<&'a mut LiquidVariableType> {
+    let mut current = variables;
+    let mut path = path.split('.');
+    while let Some(part) = path.next() {
+        if part.is_empty() {
+            continue;
+        }
+        if part.contains('[') && part.ends_with(']') {
+            todo!();
+        }
+    }
+    Some(current)
+}
 
 fn exec(instruction: &Instruction, variables: &mut LiquidVariableType, end_str: &mut String) {
     match &instruction.op_type {
@@ -21,8 +39,9 @@ fn exec(instruction: &Instruction, variables: &mut LiquidVariableType, end_str: 
         ) => {
             // Get the variable pointer in the var_tmp variabke
             // TODO: do it
-            let mut var_tmp = match manipulation_mode {
+            let mut var_tmp: &mut VarOrRaw = match manipulation_mode {
                 crate::jit::data_manipulation::DataManipulationMode::Assign => todo!(),
+                // If it's for echo, get a temporary variable
                 crate::jit::data_manipulation::DataManipulationMode::Echo => todo!(),
             };
 
