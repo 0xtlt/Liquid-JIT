a2befc759dc24db2d48db6c3b82eecfe43070bd6
Thomas <31560900+0xtlt@users.noreply.github.com>
2022-10-05 08:56:43 +0200

    remove todo

--- src/jit/fast_var.rs
+++ src/jit/fast_var.rs
@@ -91,7 +91,7 @@ pub fn create_fast_var(path: &str) -> Vec<FastVarFinder> {
 pub fn fast_var_process(instructions: &mut [Instruction]) {
     for instruction in instructions.iter_mut() {
         match &mut instruction.op_type {
-            crate::jit::types::InstructionType::Raw(_) => todo!(),
+            crate::jit::types::InstructionType::Raw(_) => {}
             crate::jit::types::InstructionType::DataManipulation(_, data, _) => {
                 if let crate::jit::types::VarOrRaw::Var(name) = data {
                     *data = crate::jit::types::VarOrRaw::FastVar(create_fast_var(name));
